dd3a15d516fd59c51f76c9b45556b0677e8c55ee
Greta Maldutyte <gretamaldutyte@gmail.com>
2021-04-20 21:48:24 +0300

    Revert "Added second button. When clicked text and button color changes."
    This reverts commit b42ab2c7bb7d285a8e33e8358a6d25ed4c22c56f.

--- app/src/main/java/com/example/fintech_lab1_greta/MainActivity.java
+++ app/src/main/java/com/example/fintech_lab1_greta/MainActivity.java
@@ -1,19 +1,13 @@
 package com.example.fintech_lab1_greta;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvMain;
-    Button btn_Change_Color;
 
 
     @Override
@@ -22,19 +16,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tvMain = findViewById(R.id.tvMain);
-        this.btn_Change_Color = findViewById(R.id.btn_Change_Color);
 
     }
 
     public void OnBtnClick(View view) {
         this.tvMain.setText(R.string.changed_label_text);
-    }
-
-    @SuppressLint("ResourceAsColor")
-    public void OnBtnClickColor(View view) {
-        this.btn_Change_Color.setTextColor(R.color.teal_200);
-        this.btn_Change_Color.setBackgroundColor(R.color.purple_200);
-        this.tvMain.setTextColor(R.color.teal_200);
-
     }
 }
