6fd42f6dab9f311cc86f29a9ef0f43689e55aa77
Greta Maldutyte <gretamaldutyte@gmail.com>
2021-04-20 11:41:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fintech_lab1_greta/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fintech_lab1_greta;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
